098c8919b044d53d0aedf85f91a1febb4525d083
j_shin <j_shin@ekc-net.jp>
2023-05-04 15:06:11 +0900

    edit comment

--- modular_rl/networks.py
+++ modular_rl/networks.py
@@ -1,8 +1,42 @@
 # -*- coding: utf-8 -*-
+"""
+ModularRL project
+
+Copyright (c) 2023 horrible
+
+PolicyNetwork is a neural network that takes in the current state of the environment as input and outputs a probability distribution over the available actions. 
+The purpose of this network is to determine the best action to take in the current state.
+
+ValueNetwork, on the other hand, 
+is a neural network that takes in the current state of the environment as input and outputs a value that represents the expected future reward that can be obtained from that state. 
+The purpose of this network is to estimate the value of a given state so that the agent can make decisions that lead to the most reward in the long term.
+
+Both networks are important components of the Proximal Policy Optimization (PPO) algorithm, 
+which is a type of reinforcement learning algorithm used for training agents to perform tasks in an environment.
+
+This software includes the following third-party libraries:
+PyTorch  (BSD-Style License): https://pytorch.org/ - Copyright (c) Facebook.
+
+"""
 import torch.nn as nn
 import torch
+
+
 class PolicyNetwork(nn.Module):
     def __init__(self, input_dim, output_dim, hidden_option='medium', num_layers=2):
+        """
+        Initialize the PolicyNetwork class with the specified input dimension, output dimension, and network settings.
+
+        :param input_dim: The dimension of the input layer.
+        :type input_dim: int
+        :param output_dim: The dimension of the output layer.
+        :type output_dim: int
+        :param hidden_option: The size of the hidden layers (options are 'small', 'medium', 'large', 'huge', or 'exhuge').
+        :type hidden_option: str
+        :param num_layers: The number of hidden layers.
+        :type num_layers: int
+        """
+
         super(PolicyNetwork, self).__init__()
 
         hidden_dim_map = {
@@ -15,7 +49,7 @@ class PolicyNetwork(nn.Module):
 
         hidden_dim = hidden_dim_map[hidden_option]
 
-        # 각 계층을 만듦
+        # Create each layer
         layers = []
         for i in range(num_layers):
             if i == 0:
@@ -28,12 +62,33 @@ class PolicyNetwork(nn.Module):
         self.net = nn.Sequential(*layers)
 
     def forward(self, x):
+        """
+        Compute a forward pass of the policy network.
+
+        :param x: The input tensor.
+        :type x: torch.Tensor
+        :return: The output tensor.
+        :rtype: torch.Tensor
+        """
+
         x = self.net(x)
         x = torch.softmax(x, dim=-1)
         return x
 
+
 class ValueNetwork(nn.Module):
     def __init__(self, input_dim, hidden_option='medium', num_layers=2):
+        """
+        Initialize the ValueNetwork class with the specified input dimension and network settings.
+
+        :param input_dim: The dimension of the input layer.
+        :type input_dim: int
+        :param hidden_option: The size of the hidden layers (options are 'small', 'medium', 'large', 'huge', or 'exhuge').
+        :type hidden_option: str
+        :param num_layers: The number of hidden layers.
+        :type num_layers: int
+        """
+
         super(ValueNetwork, self).__init__()
 
         hidden_dim_map = {
@@ -46,7 +101,7 @@ class ValueNetwork(nn.Module):
 
         hidden_dim = hidden_dim_map[hidden_option]
 
-        # 각 계층을 만듦
+        # Create each layer
         layers = []
         for i in range(num_layers):
             if i == 0:
@@ -59,5 +114,14 @@ class ValueNetwork(nn.Module):
         self.net = nn.Sequential(*layers)
 
     def forward(self, x):
+        """
+        Compute a forward pass of the value network.
+
+        :param x: The input tensor.
+        :type x: torch.Tensor
+        :return: The output tensor.
+        :rtype: torch.Tensor
+        """
+
         x = self.net(x)
         return x
--- modular_rl/tester.py
+++ modular_rl/tester.py
@@ -2,9 +2,21 @@
 from modular_rl.agents.agent_ppo import AgentPPO
 from modular_rl.settings import AgentSettings
 
+'''
+This code is a Python script that initializes and trains an instance of the AgentPPO class with default settings using the init() function. 
+Additionally, the init_modular() function is provided to demonstrate the usage of the AgentPPO class with modified settings.
+
+In init_modular(), an instance of AgentPPO is created with a modified default_modular settings dictionary. 
+The function then calls various methods of the instance, such as reset(), learn_reset(), learn_next(), and update() to demonstrate the functionality of the class. 
+The instance is then saved to a file using the save_model() method, which is not shown in the provided code.
+
+'''
+
+
 def init():
     env = AgentPPO(env=None, setting=AgentSettings.default)
     env.learn()
+
 
 def init_modular():
     env = AgentPPO(env=None, setting=AgentSettings.default_modular)
@@ -46,4 +58,4 @@ def init_modular():
 
     env.learn_close()
 
-    #env.save_model('test.pth')
+    # env.save_model('test.pth')
