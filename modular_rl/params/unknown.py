f4194c394a907967e54b290c6f20b0a361570a4e
Shin <j_shin@ekc-net.jp>
2023-05-31 23:07:52 +0900

    simulator under construction

---
+++ modular_rl/params/unknown.py
@@ -0,0 +1,12 @@
+class SimulationParameters:
+    default = {
+        'fixed_states':'fixed_states',
+        'unknown_spaces':'unknown_spaces',
+        'simulation_states':'simulation_states',
+        'excluded_states':'excluded_states',
+        'my_simulation_number':'my_simulation_number',
+        'score_table':'score_table',
+        'score_calculation_callback':'score_calculation_callback',
+        'score_column':'score',
+        'simulation_iterations':30,
+    }
